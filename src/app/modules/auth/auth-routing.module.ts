8c1c6ff5848c9b10f29ea54bc2bd5d01fb0e4c99
unknown <abishekamstrong@gmail.com>
2021-01-13 10:08:42 +0530

    added signup and login compoentns

---
+++ src/app/modules/auth/auth-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthComponent } from './auth.component';
+
+const routes: Routes = [
+  { path: '', component: AuthComponent },
+  {
+    path: 'signup',
+    loadChildren: () =>
+      import('./signup/signup.module').then((m) => m.SignupModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}

620ed84186da772a9e9c8d9a823f4f00a6d84c21
Venkatasamy <venkatasamy1991@gmail.com>
2021-01-13 23:56:36 +0530

    Header Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'homemade-ui';
+  title = 'Homemade-UI';
+  public isShow : boolean = false;
+  public layer_is_visible : boolean = false;
+  public is_show_normal  : boolean = false;
+
+  constructor()
+  {
+  }
+
+  enableSandwichMenu()
+  {
+    this.isShow = this.isShow == true ? false : true;
+    this.layer_is_visible = this.layer_is_visible == true ? false : true;
+  }
+  
+  expandCollapseSandwichSubMenu()
+  {
+    this.is_show_normal = this.is_show_normal == true ? false : true;
+  }
 }

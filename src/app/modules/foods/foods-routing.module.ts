f29f4d562019f0e589af629dce13bfe5a567d539
unknown <abishekamstrong@gmail.com>
2021-01-16 22:48:40 +0530

    food detail page is done

--- src/app/modules/foods/foods-routing.module.ts
+++ src/app/modules/foods/foods-routing.module.ts
@@ -3,10 +3,26 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { FoodsComponent } from './foods.component';
 
-const routes: Routes = [{ path: '', component: FoodsComponent }, { path: 'detail', loadChildren: () => import('./food-detail/food-detail.module').then(m => m.FoodDetailModule) }];
+const routes: Routes = [
+  { path: '', component: FoodsComponent },
+  {
+    path: 'detail/:id',
+    loadChildren: () =>
+      import('./food-detail/food-detail.module').then(
+        (m) => m.FoodDetailModule
+      ),
+  },
+  {
+    path: 'chef/:id',
+    loadChildren: () =>
+      import('./chef-detail/chef-detail.module').then(
+        (m) => m.ChefDetailModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class FoodsRoutingModule { }
+export class FoodsRoutingModule {}

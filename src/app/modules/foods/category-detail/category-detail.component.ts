9df91b0aa03af96f978849935b89ce6d737dc075
E100034 <dmuduli@yahoo.co.in>
2021-02-19 16:37:35 +0530

    new UI design implement

--- src/app/modules/foods/category-detail/category-detail.component.ts
+++ src/app/modules/foods/category-detail/category-detail.component.ts
@@ -95,21 +95,20 @@ export class CategoryDetailComponent implements OnInit {
     responsive: {
       0: {
         nav: false,
-        dots: false,
-        items: 2,
-        stagePadding: 50,
+        dots: true,
+        items: 1,
         margin: 15,
       },
       600: {
         nav: false,
-        dots: false,
-        items: 4,
+        dots: true,
+        items: 2,
         margin: 10,
       },
       768: {
         nav: false,
-        dots: false,
-        items: 4,
+        dots: true,
+        items: 3,
       },
       1024: {
         nav: true,
@@ -142,15 +141,15 @@ export class CategoryDetailComponent implements OnInit {
     this.bestSellers = [];
     this.newlyAdded = [];
     this.recommendations = [];
-    this.category = this.route.snapshot.paramMap.get('id');
+    this.category = '';
   }
 
   ngOnInit(): void {
-    this.route.params.subscribe(routeParams => {
-      this.category = this.route.snapshot.paramMap.get('id');
-      this.loadfoodDetails();
-      this.loadCuisineDetails();
-    });
+    this.category = this.route.snapshot.paramMap.get('id');
+    this.loadfoodDetails();
+    // this.loadChefDetails();
+    this.loadCuisineDetails();
+    // this.getRecentAndBest();
   }
 
   getRecentAndBest() {
@@ -273,11 +272,10 @@ export class CategoryDetailComponent implements OnInit {
   }
 
   navigateToCuisine(name: string) {
-    // this.router
-    //   .navigateByUrl('/', { skipLocationChange: true })
-    //   .then(() =>
-    //     this.router.navigate(['/', 'foods', 'category-detail', name],{replaceUrl: true})
-    //   );
-    this.router.navigate(['/', 'foods', 'category-detail', name]);
+    this.router
+      .navigateByUrl('/', { skipLocationChange: true })
+      .then(() =>
+        this.router.navigate(['/', 'foods', 'category-detail', name])
+      );
   }
 }
